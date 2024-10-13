34168b40647084f54d521ade28b9e1630c1a3f5e
uttamsojitra1204 <121995198+uttamsojitra1204@users.noreply.github.com>
2024-10-13 15:29:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
-'''
-Camera Classifier v0.1 Alpha
-Copyright (c) NeuralNine
-
-Instagram: @neuralnine
-YouTube: NeuralNine
-Website: www.neuralnine.com
-'''
-
 import app
 
 def main():
-    app.App(window_title="Camera Classifier v0.1 Alpha")
+    app.App(window_title="Camera Classifier - 1.o")
 
 if __name__ == "__main__":
     main()
